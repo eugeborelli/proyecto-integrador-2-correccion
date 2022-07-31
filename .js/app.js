639f0f86b48c9f3116fad67a02bfc049ac932527
eugeborelli <eugeborelli@gmail.com>
2022-07-30 22:43:08 -0300

    correccion de entrega

---
+++ .js/app.js
@@ -0,0 +1,36 @@
+// tipos de datos
+/* 
+let nombreCompleto = "Ada" ;
+
+console.log (nombreCompleto);
+
+
+// Operaciones matematicas
+let resultado = null;
+
+let num1 = 50;
+let num2 = 10;
+
+resultado = num1 + num2;
+
+
+console.log(resultado);
+
+
+// Mostramos los valores en la consola */
+
+const menuItems = document.querySelectorAll('.menu-item');
+const menuBtn = document.querySelector('#menu-button');
+
+menuBtn.addEventListener('click', function(){
+    document.body.classList.toggle('mobile-menu-active');
+});
+
+menuItems.forEach(function(item) {
+item.addEventListener('click', function() {  
+    const currentItems = document.querySelector('.active');
+    currentItems.classList.remove('active');
+    this.classList.add ('active');
+ }); 
+    
+});
